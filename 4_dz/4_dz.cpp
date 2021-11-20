838ca4bb3ecdd84d0cedb361f513f5d62302708e
Hellhound <hardoneloves@gmail.com>
2021-11-20 13:38:08 +0300

    Добавьте файлы проекта.

---
+++ 4_dz/4_dz.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+	int a, b = 0;
+	std::cin >> a;
+	for (int i = 1; i <= a; i++) {
+		if (a % i == 0) b++;
+	}
+	if (b == 2) std::cout << "prost";
+	else std::cout << "neprost";
+
+	return 0;
+}
